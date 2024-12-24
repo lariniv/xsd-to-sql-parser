d7932754f307ddc63eecb8f613382e1bd09b3dc2
Ivan Larin <n.larivanko@gmail.com>
2024-12-24 19:14:05 +0200

    feat: add new constants

--- src/consts.py
+++ src/consts.py
@@ -1,5 +1,8 @@
 path = "https://bank.gov.ua/NBUStatService/v1/statdirectory/report/f3wx.xsd?reportdate=20250101"
 
+output_file = 'output.sql'
+table_name = 'XSD_PARSE'
+
 id_param = 0
 
 src_db_owner = "src_db_ownership"
